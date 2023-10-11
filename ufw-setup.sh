cb29f6a64a89951a374276bbdf45e76671f26a70
Justin Healey <jjheals@gmail.com>
2023-10-11 09:51:09 -0400

    added http

--- ufw-setup.sh
+++ ufw-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# SSH 
 enable_ssh=false    # Flag to enable SSH
 ssh_from=false      # Var for the SSH "from_ip"
+
+# HTTP/HTTPS 
+enable_http=false
+force_https=false
+interface="eth0"
 
 # Define a regex to identify a valid IPv4 address for the
 # from_ip parameter
@@ -9,12 +15,23 @@ ipv4_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
 
 # Check what to enable
 for arg in "$@"; do
-    # Check SSH
-    if [ "$arg" == "-ssh" ]; then
+    
+    # Enable SSH 
+    if [[ "$arg" == "-ssh" ]]; then 
         enable_ssh=true
-    elif [[ $arg =~ $ipv4_regex ]]; then
-        ssh_from="$arg"
-    fi 
+        shift 
+        if [[ $# -gt 0 && $# =~ ipv4_regex ]]; then 
+            ssh_from="$1"
+            shift
+        fi
+    elif [[ "$arg" == "-http" ]]
+        enable_http=true
+        shift 
+        if [[ $# -gt 0 && $# == "force" ]]; then 
+            force_https=true
+            shift      
+        fi  
+    fi
 done
 
 sudo apt -y update && sudo apt upgrade
@@ -22,12 +39,28 @@ sudo apt -y install ufw
 
 sudo ufw enable 
 
+# Enabling/allowing based on args
+# SSH
 if [[ $enable_ssh ]]; then
     if [[ $ssh_from =~ $ipv4_regex ]]; then
         sudo ufw allow from $ssh_from to any port 22
     else 
         sudo ufw allow 22
+    fi
+fi 
+
+# HTTP/HTTPS
+if $enable_http; then
+    sudo ufw allow 80
+    sudo ufw allow 443
+
+    if $force_https; then
+        # Create rule to redirect HTTP to HTTPs using iptables
+        sudo iptables -A PREROUTING -t nat -i $interface -p tcp --dport 80 -j REDIRECT --to-port 443
+        sudo sh -c 'iptables-save > /etc/iptables/rules.v4'
+    fi
 fi
+
 
 echo "DONE with UFW setup."
 sudo systemctl status ufw
